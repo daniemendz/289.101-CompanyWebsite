d486bb8ecd65161676fc4f7dd3b7358343ef2786
daniemendz <daniellemendz@gmail.com>
2019-05-21 16:23:05 +1200

    Update script.js

--- javascript_intro/collision_detection/script.js
+++ javascript_intro/collision_detection/script.js
@@ -15,6 +15,44 @@ var puck_y = 210;
 var puck =  document.getElementById('puck');
 var speed = 50
 
+function log(){
+    console.log('x=' + puck_x + ' y=' + puck_y);
+}
+
+function checkCollision(){
+    //has the puck landed in the hole?
+    //vertical edge of hole
+    if (puck_x > 250 && puck_x < 350 && puck_y > 150 && puck_y < 250){
+        console.log("AAAHH");
+    }
+}
+
+function teleport(){
+    if (puck_x < 0){
+        console.log("WHY");
+        puck_x = 580
+        puck.style.left= puck_x + "px";
+    }
+    
+    if (puck_x > 600){
+        console.log("WHY");
+        puck_x = 10
+        puck.style.left= puck_x + "px";
+    }
+    
+    if (puck_y < 0){
+        console.log("WHY");
+        puck_y = 380
+        puck.style.top= puck_y + "px";
+    }
+    
+    if (puck_y > 400){
+        console.log("WHY");
+        puck_y = 10
+        puck.style.top= puck_y + "px";
+    }
+}
+
 
 puck.style.left = puck_x + 'px';
 puck.style.top = puck_y + 'px';
@@ -23,18 +61,31 @@ puck.style.top = puck_y + 'px';
 document.getElementById('left').addEventListener('click', function(){
     puck_x -= speed;
     puck.style.left= puck_x + "px";
+    log()
+    checkCollision()
+    teleport()
 });
 document.getElementById('right').addEventListener('click', function(){
     puck_x += speed;
     puck.style.left= puck_x + "px";
+    log()
+    checkCollision()
+    teleport()
 });
 
 document.getElementById('up').addEventListener('click', function(){
     puck_y -= speed;
     puck.style.top= puck_y + "px";
+    log()
+    checkCollision()
+    teleport()
 });
 document.getElementById('down').addEventListener('click', function(){
     puck_y += speed;
     puck.style.top= puck_y + "px";
+    log()
+    checkCollision()
+    teleport()
 });
+
 
