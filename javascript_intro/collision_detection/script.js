f90a9bfcbbba4914806e3d3448a70efe84a56dff
daniemendz <daniellemendz@gmail.com>
2019-05-21 15:53:41 +1200

    Update script.js

--- javascript_intro/collision_detection/script.js
+++ javascript_intro/collision_detection/script.js
@@ -9,3 +9,32 @@ Next, add the following features:
 - the puck 'teleports' to the opposite boundary when it crosses one (think asteroids/snake)
 
 */
+
+var puck_x = 210;
+var puck_y = 210;
+var puck =  document.getElementById('puck');
+var speed = 50
+
+
+puck.style.left = puck_x + 'px';
+puck.style.top = puck_y + 'px';
+
+
+document.getElementById('left').addEventListener('click', function(){
+    puck_x -= speed;
+    puck.style.left= puck_x + "px";
+});
+document.getElementById('right').addEventListener('click', function(){
+    puck_x += speed;
+    puck.style.left= puck_x + "px";
+});
+
+document.getElementById('up').addEventListener('click', function(){
+    puck_y -= speed;
+    puck.style.top= puck_y + "px";
+});
+document.getElementById('down').addEventListener('click', function(){
+    puck_y += speed;
+    puck.style.top= puck_y + "px";
+});
+
